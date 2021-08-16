4cc2e6f35bc75871488fdb9fbf85fe080d84a008
Laura Endreß <laura.endress@syntaxphoenix.com>
2021-08-16 21:11:00 +0200

    Api done

--- legacy-api/src/main/java/net/sourcewriters/spigot/rwg/legacy/api/schematic/SchematicConverter.java
+++ legacy-api/src/main/java/net/sourcewriters/spigot/rwg/legacy/api/schematic/SchematicConverter.java
@@ -7,6 +7,8 @@ import java.util.Collections;
 import org.bukkit.plugin.Plugin;
 
 public abstract class SchematicConverter {
+    
+    private final long id;
 
     private final Plugin plugin;
     private final ArrayList<String> extensions = new ArrayList<>();
@@ -14,10 +16,15 @@ public abstract class SchematicConverter {
     public SchematicConverter(Plugin plugin, String... extensions) {
         this.plugin = plugin;
         Collections.addAll(this.extensions, extensions);
+        this.id = plugin.getName().hashCode() + (extensions.hashCode() * 32);
     }
     
     public final Plugin getPlugin() {
         return plugin;
+    }
+    
+    public final long getId() {
+        return id;
     }
     
     public final boolean canConvert(String extension) {
